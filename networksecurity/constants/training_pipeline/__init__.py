461b31cdd73f27e700431925a888d2e2520ffe72
Palak Mathur <palakmathur2811@gmail.com>
2025-07-16 23:39:41 +0530

    data transformation

--- networksecurity/constants/training_pipeline/__init__.py
+++ networksecurity/constants/training_pipeline/__init__.py
@@ -51,8 +51,8 @@ DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR: str = "transformed"
 DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR: str = "transformed_object"
 
 ## kkn imputer to replace nan values
-DATA_TRANSFORMATION_IMPUTER_PARAMS: dict = {
-    "missing_values": np.nan,
+DATA_TRANSFORMATION_IMPUTER_PARAMS: dict = { #imputer for replacing nan value
+    "missing_values": np.nan, 
     "n_neighbors": 3,
     "weights": "uniform",
 }

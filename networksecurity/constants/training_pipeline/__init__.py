636376ed30fbbef7135e7cfdd802c9377234a954
Palak Mathur <palakmathur2811@gmail.com>
2025-07-19 00:18:53 +0530

    model trainer including mlflow

--- networksecurity/constants/training_pipeline/__init__.py
+++ networksecurity/constants/training_pipeline/__init__.py
@@ -68,7 +68,7 @@ Model Trainer ralated constant start with MODE TRAINER VAR NAME
 MODEL_TRAINER_DIR_NAME: str = "model_trainer"
 MODEL_TRAINER_TRAINED_MODEL_DIR: str = "trained_model"
 MODEL_TRAINER_TRAINED_MODEL_NAME: str = "model.pkl"
-MODEL_TRAINER_EXPECTED_SCORE: float = 0.6
-MODEL_TRAINER_OVER_FIITING_UNDER_FITTING_THRESHOLD: float = 0.05
+MODEL_TRAINER_EXPECTED_SCORE: float = 0.6 # at least 60 percent accuracy should be there for the model to be considered good
+MODEL_TRAINER_OVER_FIITING_UNDER_FITTING_THRESHOLD: float = 0.05 # this much diffrence between modeles ok 
 
 TRAINING_BUCKET_NAME = "netwworksecurity"
